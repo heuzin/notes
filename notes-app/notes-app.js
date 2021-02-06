3b14c9bac940cbe97c285b77140f1f0e0a2148a7
heuzin <matheusqs.arq@gmail.com>
2021-02-05 22:27:31 -0500

    filtering search notes

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,30 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = (notes, filters) => {
+    // filtering notes based on search text
+    // if notes title includes filters search text
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    // showing each note on screen based on filtered notes
+    filteredNotes.forEach((note) => {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#search-text').addEventListener('input', e => {
-    console.log(e.target.value)
+    // updating filters search text to e.target.value, wich is what is being typed on input
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
