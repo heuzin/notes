60aa1b51ae66c8192250bc8bafe5ac14bc2bde03
heuzin <matheusqs.arq@gmail.com>
2021-02-05 21:13:30 -0500

    new notes array

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,10 @@
-console.log('working')
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
