15b2f8cf6ba6d760ab5280463b0d39bc7cd18e09
heuzin <matheusqs.arq@gmail.com>
2021-02-06 23:28:04 -0500

    setting up ids for each note

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -20,9 +20,11 @@ const generateNoteDOM = (note) => {
     const textEl = document.createElement('span')
     const button = document.createElement('button')
 
+    // Setup the remove note button
     button.textContent = 'x'
     noteEl.appendChild(button)
 
+    // Setup the note title text
     if (note.title.length > 0) {
         textEl.textContent = note.title
     } else {
