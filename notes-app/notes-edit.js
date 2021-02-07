079d06095c54066305ba2106bcc4a33f0392966b
heuzin <matheusqs.arq@gmail.com>
2021-02-07 18:30:55 -0500

    syncing data for edit page acros multiple pages

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -3,8 +3,8 @@ const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 // Gets id from location hash (browsers link) startind from index 1, index 0 is the hash #
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find((note) => {
+let notes = getSavedNotes()
+let note = notes.find((note) => {
     return note.id === noteId
 })
 
@@ -29,4 +29,20 @@ removeElement.addEventListener('click', (e) => {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        let note = notes.find((note) => {
+            return note.id === noteId
+        })
+        
+        if (note === undefined) {
+            location.assign('/index.html')
+        }
+        
+        titleElement.value = note.title
+        bodyElement.value = note.body 
+    }
 })
