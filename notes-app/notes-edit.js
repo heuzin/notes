04e20a4d9dabcab0128bc2ce3e0f02c1d20af9eb
heuzin <matheusqs.arq@gmail.com>
2021-02-07 18:21:40 -0500

    editin title and body, and removing button for removing note

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
 // Gets id from location hash (browsers link) startind from index 1, index 0 is the hash #
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
@@ -7,4 +10,23 @@ const note = notes.find((note) => {
 
 if (note === undefined) {
     location.assign('/index.html')
-} 
+}
+
+titleElement.value = note.title
+bodyElement.value = note.body 
+
+titleElement.addEventListener('input', (e) => {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input', (e) => {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeElement.addEventListener('click', (e) => {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
