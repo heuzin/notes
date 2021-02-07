4fb576cc587f769d3c3876044bdcd7980a96561e
heuzin <matheusqs.arq@gmail.com>
2021-02-07 18:33:15 -0500

    syncing data for home page

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -22,3 +22,10 @@ document.querySelector('#search-text').addEventListener('input', e => {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
+})
