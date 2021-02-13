577573ec770df54648aa108fde5eaed599cb7f59
heuzin <matheusqs.arq@gmail.com>
2021-02-13 00:40:00 -0300

    setting up filters.js for seachText and sortBy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { remove } from 'lodash'
 import { getNotes, createNote, removeNote, updateNote } from './notes'
-
+import { getFilters, setFilters } from './filters'
 // console.log(getNotes())
 // createNote()
-updateNote('43cde408-cd31-4175-9e95-d3e279858c82', {
-    title: 'new title'
+// updateNote('43cde408-cd31-4175-9e95-d3e279858c82', {
+//     title: 'new title'
+// })
+console.log(getFilters())
+setFilters({
+    searchText: 'office',
+    sortBy: 'byCreated'
 })
-console.log(getNotes())
+console.log(getFilters())
--- src/notes.js
+++ src/notes.js
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
 import moment from 'moment'
-import { valueOf } from 'detect-node'
 
 let notes = []
 
